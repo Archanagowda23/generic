501de0f458d1071009312717fd24817ba93c42d4
Archanagowda23 <archanar249@gmail.com>
2022-03-21 22:02:05 +0530

    added array of different data types

--- src/com/bridgelabz/generic/GenericDemo.java
+++ src/com/bridgelabz/generic/GenericDemo.java
@@ -4,5 +4,18 @@ public class GenericDemo {
 
     public static void main(String[] args) {
         System.out.println("Welcome to generic program");
+        Generic<String> g1 = new Generic<String>("Archana");
+        g1.show();
+        System.out.println(g1.getObject());
+
+        Generic<Integer> g2 = new Generic<Integer>(10);
+        g2.show();
+        System.out.println(g2.getObject());
+
+        Generic<Double> g3 = new Generic<Double>(10.5);
+        g3.show();
+        System.out.println(g3.getObject());
     }
+
 }
+
